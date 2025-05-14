e859ef2e5061ce8c19e80162c9449f28c6845267
roeepaz <roeepaz333531@gmail.com>
2025-05-14 13:20:26 +0300

    fix to the files names

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import BudgetApp from './BudgetApp';
-import ExpenseTracker from './home';
-import HomePage from './homePage';
+import ExpenseTracker from './ExpenseTracke';
+import HomePage from './HomePage';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
